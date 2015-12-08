151eab4c7c8135763c91e99f8c56a7a61ce6755a
Alex Jay Hyer <TheOneHyer@linux-3tt7.site>
2015-12-08 12:34:22 -0700

    resource_alerter now daemonizes properly w/ small edits to python-daemon

--- resource_alerter/resource_alerter.py
+++ resource_alerter/resource_alerter.py
@@ -5,18 +5,18 @@
 
 """
 
-import argparse
 from daemon import runner
 import logging
 import logging.config
 from pkg_resources import resource_string
 import sys
+import time
 import yaml
 
 __version__ = '0.0.0a1'
 
 
-class ResourceAlerter():
+class ResourceAlerter:
     def __init__(self):
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/null'
@@ -31,17 +31,28 @@ class ResourceAlerter():
 
 
 if __name__ == '__main__':
-    # TODO: Put argparse code here
-
     resource_alerter = ResourceAlerter()
-    with open(resource_string(__file__, 'resource_alerter.logging.conf'),
+    # with open(resource_string(__file__, 'resource_alerter.logging.conf'),
+    with open('/usr/lib/python3.4/site-packages'
+              '/resource_alerter/resource_alerter.logging.conf',
               'rU') as config_handle:
         config_dict = yaml.load(config_handle)
     logging.config.dictConfig(config_dict)
+    debug_logger = logging.getLogger('debug_logger')
+    info_logger = logging.getLogger('info_logger')
+    warning_logger = logging.getLogger('warning_logger')
+    error_logger = logging.getLogger('error_logger')
+    critical_logger = logging.getLogger('critical_logger')
+    loggers = [debug_logger, info_logger, warning_logger, error_logger,
+               critical_logger]
+    files_to_preserve = []
+    for logger in loggers:
+        for i in range(len(logger.handlers)):
+            file_stream = logger.handlers[i].stream
+            if file_stream not in files_to_preserve:
+                files_to_preserve.append(file_stream)
     daemon_runner = runner.DaemonRunner(resource_alerter)
-    daemon_runner.daemon_context.files_preserve = \
-        [config_dict['handlers'][handler]['filename'] for handler in
-         config_dict['handlers'].keys()]
+    daemon_runner.daemon_context.files_preserve = files_to_preserve
     daemon_runner.do_action()
 
     sys.exit(0)
