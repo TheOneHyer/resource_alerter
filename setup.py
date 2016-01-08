74f72a07b45fe264ac461f81598188cb1eae55d4
TheOneHyer <theonehyer@gmail.com>
2016-01-07 17:16:07 -0700

    Added package data to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='resource_alerter',
-      version='0.0.0b8',
+      version='0.0.0b9',
       description='monitors system resources and alerts users to high usage',
       classifiers=[
           'Development Status :: 4 - Beta',
@@ -20,11 +20,14 @@ setup(name='resource_alerter',
       keywords='daemon resource alerter monitor monitoring log logging',
       url='https://github.com/TheOneHyer/resource_alerter',
       download_url='https://github.com/TheOneHyer/resource_alerter/tarball/'
-                    + '0.0.0b8',
+                    + '0.0.0b9',
       author='Alex Hyer',
       author_email='theonehyer@gmail.com',
       license='GPLv3',
       packages=find_packages(),
+      package_data={
+          'resource_alerter': ['*.conf']
+      },
       include_package_data=True,
       zip_safe=False,
       scripts=[
